3f949513d7f2a3bd14ed114b5841c8443f2fa422
csimoes-eto <csimoes10@gmail.com>
2022-12-14 01:41:43 +0000

    all queries good (7 uses limit)

--- queries.sql
+++ queries.sql
@@ -6,152 +6,137 @@ SELECT
     l.name,
     p.area
 FROM
-    Landowner l,
-    Property p
+    Landowner l
+    INNER JOIN Property p ON l.lo_id = p.lo_id
 WHERE
-    l.lo_id = p.lo_id
-    AND l.yearofbirth BETWEEN 1992
+    l.yearofbirth BETWEEN 1992
     AND 2004
     AND p.area > 2000;
 
 --2
 SELECT
     DISTINCT l.vat,
-    l.name,
-    f.dstart
+    l.name
 FROM
-    Landowner l,
-    Stand s,
-    Fire f,
-    Property p,
-    StandFire fs
+    Landowner l
+    INNER JOIN Property p ON l.lo_id = p.lo_id
+    INNER JOIN StandFire sf ON sf.pr_id = p.prop_id
+    INNER JOIN Fire f ON f.f_id = sf.f_id
 WHERE
-    l.lo_id = p.lo_id
-    AND p.prop_id = fs.pr_id
-    AND fs.f_id = f.f_id
-    AND f.dstart >= "2021-01-01T00:00:00.000"
-    AND f.dstart <= "2021-12-31T23:59:59:999";
+    YEAR(f.dstart) = 2021;
 
 --3
 SELECT
     l.lo_id,
-    l.vat,
     l.name,
-    st.prop_id,
-    st.st_id,
-    st.spec_id
+    l.vat
 FROM
-    Landowner l,
-    Property p,
-    Stand st
+    Landowner l
+    INNER JOIN Property p ON p.lo_id = l.lo_id
+    INNER JOIN Stand st on st.prop_id = p.prop_id
+    INNER JOIN Species sp on sp.spec_id = st.spec_id
 WHERE
-    st.spec_id = 1
-    AND st.prop_id = p.prop_id
-    AND p.lo_id = l.lo_id
+    sp.name = "Cork Oak"
     AND l.lo_id NOT IN (
         SELECT
-            l.lo_id
+            l2.lo_id
         FROM
-            Landowner l,
-            Property p,
-            Stand st
+            Landowner l2
+            INNER JOIN Property p2 ON p2.lo_id = l2.lo_id
+            INNER JOIN Stand st2 ON st2.prop_id = p2.prop_id
+            INNER JOIN Species sp ON sp.spec_id = st2.spec_id
         WHERE
-            st.spec_id = 2
-            AND st.prop_id = p.prop_id
-            AND p.lo_id = l.lo_id
+            sp.name = "Eucalyptus"
     );
 
 --4
 SELECT
-    l.lo_id,
-    l.vat,
+    DISTINCT l.vat,
     l.name
 FROM
-    Landowner l,
-    Property p,
-    Stand s
+    Landowner l
+    LEFT OUTER JOIN Property p ON l.lo_id = p.lo_id
+    LEFT OUTER JOIN StandFire sf ON p.prop_id = sf.pr_id
 WHERE
-    l.lo_id = p.lo_id
-    AND p.prop_id = s.prop_id
-    AND s.st_id NOT IN (
-        SELECT
-            sf.st_id
-        FROM
-            StandFire sf
-    );
+    sf.f_id IS NULL;
 
 --5
 SELECT
-    p.prop_id,
-    CAST(SUBSTRING(f.dstart, 1, 4) AS INT) AS year,
-    SUM(sf.area_burned) as "Total Burnt"
+    sf.pr_id as property_id,
+    YEAR(f.dstart) as year,
+    SUM(sf.area_burned) as Total_Burnt
 FROM
-    Property p,
-    Stand st,
-    Fire f,
     StandFire sf
-WHERE
-    sf.pr_id = p.prop_id
-    AND sf.f_id = f.f_id
-    AND sf.st_id = st.st_id
+    INNER JOIN Fire f ON f.f_id = sf.f_id
 GROUP BY
     year,
-    p.prop_id;
+    property_id;
 
 --6
 SELECT
-    DISTINCT s.st_id
+    b.prop_id1,
+    b.st_id1,
+    b.prop_id2,
+    b.st_id2,
+    f.f_id,
+    YEAR(f.dstart) AS year
 FROM
-    Stand s,
-    Border b,
-    Fire f
+    Border b
+    INNER JOIN StandFire sf ON (
+        sf.pr_id = b.prop_id1
+        AND sf.st_id = b.st_id1
+    )
+    OR (
+        sf.pr_id = b.prop_id2
+        AND sf.st_id = b.st_id2
+    )
+    INNER JOIN Fire f ON f.f_id = sf.f_id
 WHERE
-    s.st_id = b.st_id1
-    OR s.st_id = b.st_id2 IN (
-        SELECT
-            s2.st_id
-        FROM
-            StandFire sf,
-            Fire f2,
-            Stand s2
-        WHERE
-            f2.ftype = "C"
-            AND sf.f_id = f2.f_id
-            AND sf.st_id = s2.st_id
-    );
+    f.ftype = "C"
+    AND YEAR(f.dstart) = 2021;
 
---7 grouping ? tambem ainda nao sei
--- SELECT
---     CAST(SUBSTRING(f.dstart, 1, 4) AS INT) AS "year",
---     l.name,
---     sf.area_burned
--- FROM
---     Property p,
---     Stand st,
---     Fire f,
---     StandFire sf,
---     Landowner l
--- WHERE
---     sf.pr_id = p.prop_id
---     AND sf.f_id = f.f_id
---     AND sf.st_id = st.st_id
--- GROUP BY
---     year,
---     l.name;
+--7
+SELECT
+    YEAR(f.dstart) as year,
+    l.name,
+    SUM(sf.area_burned) as area_burnt_in_year
+FROM
+    StandFire sf
+    INNER JOIN Fire f ON f.f_id = sf.f_id
+    INNER JOIN Property p ON p.prop_id = sf.pr_id
+    INNER JOIN Landowner l ON l.lo_id = p.lo_id
+GROUP BY
+    year,
+    l.lo_id
+HAVING
+    area_burnt_in_year = (
+        SELECT
+            SUM(sf2.area_burned)
+        FROM
+            StandFire sf2
+            INNER JOIN Fire f2 ON f2.f_id = sf2.f_id
+            INNER JOIN Property p2 ON p2.prop_id = sf2.pr_id
+            INNER JOIN Landowner l2 ON l2.lo_id = p2.lo_id
+        WHERE
+            year(f2.dstart) = year
+        GROUP BY
+            l2.lo_id
+        ORDER BY
+            area_burned DESC
+        LIMIT
+            1
+    );
 
 --8
 SELECT
-    DISTINCT l.lo_id,
+    l.lo_id,
     l.name,
     l.vat
 FROM
-    Landowner l,
-    Property p,
-    Stand st,
-    Species sp
-WHERE
-    st.prop_id = p.prop_id
-    AND p.lo_id = l.lo_id
+    Stand st
+    INNER JOIN Species sp ON sp.spec_id = st.spec_id
+    INNER JOIN Property p ON p.prop_id = st.prop_id
+    INNER JOIN Landowner l ON l.lo_id = p.lo_id
 GROUP BY
     l.lo_id
 HAVING
@@ -159,5 +144,5 @@ HAVING
         SELECT
             COUNT(*)
         FROM
-            Species sp2
+            Species
     );
